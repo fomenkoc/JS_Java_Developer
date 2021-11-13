6faa4882675f6227f7caf8fc9a409c6008340334
ghost <ghost@SkyNet>
2021-11-13 10:27:13 +0200

    homework 6

---
+++ src/homeworks/homework-06/hw06AppFunc.js
@@ -0,0 +1,50 @@
+"use strict";
+
+function Robot(){
+	
+	this.workMsg = "Я Robot – я просто працюю";
+	this.work = function(){
+		console.log(this.workMsg);
+	};
+}
+
+function CoffeeRobot(){
+
+	Robot.call(this);
+	this.workMsg = "Я CoffeeRobot – я варю каву";
+}
+
+function RobotDancer(){
+
+	Robot.call(this);
+	this.workMsg = "Я RobotDancer – я просто танцюю";
+}
+
+function RobotCoocker(){
+
+	Robot.call(this);
+	this.workMsg = "Я RobotCoocker – я просто готую";
+}
+
+
+
+
+let robot = new Robot();
+robot.work();
+
+let coffeeRobot = new CoffeeRobot();
+coffeeRobot.work();
+
+let robotCook = new RobotCoocker();
+robotCook.work();
+
+let robotDancer = new RobotDancer();
+robotDancer.work();
+console.log();
+console.log("for loop:");
+
+let array = [robot, coffeeRobot, robotCook, robotDancer];
+
+for (let i = 0; i < array.length; i++){
+	array[i].work();
+}
